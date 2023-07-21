19816624d8229fd4874d43a941352d559d1b7342
aleksei-shvetz <al.sv.dev@gmail.com>
2023-07-21 18:39:40 +0300

    Create 'help' generation in cli

--- gendiff.js
+++ gendiff.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 import { Command } from 'commander';
 
 const program = new Command();
@@ -6,10 +7,6 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .helpOption('-h', '--help');
-
-program.command('gendiff')
-  .option('-V', '--version', 'output the version number')
-  .option('-h', '--help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information');
 
 program.parse();
