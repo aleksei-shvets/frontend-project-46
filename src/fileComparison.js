32a6c6391539752503729300a0deb747aaf69605
aleksei-shvetz <al.sv.dev@gmail.com>
2023-08-07 20:57:30 +0300

    Add 'hasPath' function

--- src/fileComparison.js
+++ src/fileComparison.js
@@ -3,15 +3,27 @@ import { existsSync, readFileSync } from 'node:fs';
 import path from 'node:path';
 import fileParse from './fileParse.js';
 
+const hasPath = (path) => {
+  if (existsSync(path)) return true;
+  throw new Error(`${path} <<< this file not found`);
+};
+
 export default (file1, file2) => {
   const result = {};
 
   const readFile = (file) => readFileSync(path.resolve(file), 'utf-8');
 
+  let fileContent1;
+  let fileContent2;
   const extension1 = path.extname(file1);
   const extension2 = path.extname(file2);
-  const fileContent1 = fileParse(readFile(file1), extension1);
-  const fileContent2 = fileParse(readFile(file2), extension2);
+
+  if (hasPath(file1)) {
+    fileContent1 = fileParse(readFile(file1), extension1);
+  }
+  if (hasPath(file2)) {
+    fileContent2 = fileParse(readFile(file2), extension2);
+  }
 
   _.forIn(fileContent1, (value, key) => {
     const newMinusKey = `- ${key}`;
