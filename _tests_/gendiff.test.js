eaa555e6120393f5ffde11fadb5be79847a7ca0f
aleksei-shvetz <al.sv.dev@gmail.com>
2023-08-07 12:11:26 +0300

    Corrected test

--- _tests_/gendiff.test.js
+++ _tests_/gendiff.test.js
@@ -1,2 +1,23 @@
-import gendiff from '../bin/gendiff.js';
+import { readFileSync } from 'fs';
+import fileComparison from '../bin/gendiff.js';
 
+const path = require('path');
+
+const getFullPath = (filename) => path.resolve(__dirname, '__fixtures__', filename);
+
+const output = {
+  host: 'hexlet.io',
+  '- timeout': 50,
+  '+ timeout': 20,
+  '- proxy': '123.234.53.22',
+  '- follow': false,
+  '+ verbose': true,
+};
+
+test('gendiff output', () => {
+  const filePath1 = getFullPath('file1.json');
+  const filePath2 = getFullPath('file2.json');
+  expect(() => {
+    fileComparison(filePath1, filePath2);
+  }).toEqual(output);
+});
