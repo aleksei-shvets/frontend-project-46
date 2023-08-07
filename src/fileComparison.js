7aea28f061270747b4625c2b4af340b5552ee997
aleksei-shvetz <al.sv.dev@gmail.com>
2023-08-07 21:14:30 +0300

    Refactoring fileComparison.js

--- src/fileComparison.js
+++ src/fileComparison.js
@@ -3,26 +3,26 @@ import { existsSync, readFileSync } from 'node:fs';
 import path from 'node:path';
 import fileParse from './fileParse.js';
 
-const hasPath = (path) => {
-  if (existsSync(path)) return true;
-  throw new Error(`${path} <<< this file not found`);
-};
+const readFile = (file) => readFileSync(path.resolve(file), 'utf-8');
 
 export default (file1, file2) => {
   const result = {};
-
-  const readFile = (file) => readFileSync(path.resolve(file), 'utf-8');
 
   let fileContent1;
   let fileContent2;
   const extension1 = path.extname(file1);
   const extension2 = path.extname(file2);
 
-  if (hasPath(file1)) {
+  if (existsSync(file1)) {
     fileContent1 = fileParse(readFile(file1), extension1);
+  } else {
+    throw new Error(`${file1} <<< this file not found`);
   }
-  if (hasPath(file2)) {
+
+  if (existsSync(file2)) {
     fileContent2 = fileParse(readFile(file2), extension2);
+  } else {
+    throw new Error(`${file2} <<< this file not found`);
   }
 
   _.forIn(fileContent1, (value, key) => {
