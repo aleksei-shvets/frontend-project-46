355a0e66390f46fab2e90eeef5cc88fe48cf842c
aleksei-shvetz <al.sv.dev@gmail.com>
2023-08-08 12:33:35 +0300

    Corrected path generation in test

--- _tests_/gendiff.test.js
+++ _tests_/gendiff.test.js
@@ -1,4 +1,14 @@
+/* eslint-disable no-underscore-dangle */
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import fs from 'fs';
 import fileComparison from '../src/fileComparison.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => getFixturePath(filename);
 
 const output = {
   host: 'hexlet.io',
@@ -11,7 +21,7 @@ const output = {
 const equal = JSON.stringify(output, ' ', 2);
 
 test('gendiff output', () => {
-  const filePath1 = './__fixtures__/file1.json';
-  const filePath2 = './__fixtures__/file2.json';
+  const filePath1 = readFile('file1.json');
+  const filePath2 = readFile('file2.json');
   expect(fileComparison(filePath1, filePath2)).toEqual(equal);
 });
