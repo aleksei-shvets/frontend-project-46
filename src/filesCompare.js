19093df6c508d7e3e74a974e8272248442655094
aleksei-shvetz <al.sv.dev@gmail.com>
2023-08-12 17:37:21 +0300

    Fixed compare function

--- src/filesCompare.js
+++ src/filesCompare.js
@@ -1,52 +1,31 @@
 import _ from 'lodash';
-import { existsSync, readFileSync } from 'node:fs';
+import { readFileSync } from 'node:fs';
 import path from 'node:path';
 import fileParse from './fileParse.js';
 
-const readFile = (file) => readFileSync(path.resolve(file), 'utf-8');
-
-const hasPath = (filePath) => {
-  if (existsSync(filePath)) return true;
-  throw new Error(`${filePath} <<< this file not found`);
-};
+const readFile = (file) => readFileSync(path.resolve(file), 'utf8');
 
 export default (file1, file2) => {
-  const result = {};
-
-  let fileContent1;
-  let fileContent2;
   const extension1 = path.extname(file1);
   const extension2 = path.extname(file2);
+  const fileContent1 = fileParse(readFile(file1), extension1);
+  const fileContent2 = fileParse(readFile(file2), extension2);
+  const keys = Object.keys({ ...fileContent1, ...fileContent2 });
 
-  if (hasPath(file1)) {
-    fileContent1 = fileParse(readFile(file1), extension1);
-  }
-  if (hasPath(file2)) {
-    fileContent2 = fileParse(readFile(file2), extension2);
-  }
-
-  _.forIn(fileContent1, (value, key) => {
-    const newMinusKey = `- ${key}`;
-    const newPlusKey = `+ ${key}`;
-    if (!Object.hasOwn(fileContent2, key)) {
-      result[newMinusKey] = value;
-    }
-
-    if (Object.hasOwn(fileContent2, key) && value === fileContent2[key]) {
-      result[key] = value;
-    }
-
-    if (Object.hasOwn(fileContent2, key) && value !== fileContent2[key]) {
-      result[newMinusKey] = fileContent1[key];
-      result[newPlusKey] = fileContent2[key];
-    }
-  });
-
-  _.forIn(fileContent2, (value, key) => {
-    const newPlusKey = `+ ${key}`;
-    if (!Object.hasOwn(fileContent1, key)) {
-      result[newPlusKey] = value;
-    }
-  });
-  return JSON.stringify(result, ' ', 2);
+  const result = _.sortBy(keys, (key) => key)
+    .map((key) => {
+      if (Object.hasOwn(fileContent1, key) && Object.hasOwn(fileContent2, key)) {
+        if (fileContent1[key] === fileContent2[key]) {
+          return `  ${key}: ${fileContent1[key]}`;
+        }
+        if (fileContent1[key] !== fileContent2[key]) {
+          return `  - ${key}: ${fileContent1[key]}\n  + ${key}:${fileContent2[key]}`;
+        }
+      }
+      if (Object.hasOwn(fileContent1, key) && !Object.hasOwn(fileContent2, key)) {
+        return `  - ${key}: ${fileContent1[key]}`;
+      }
+      return `  + ${key}: ${fileContent2[key]}`;
+    }).join('\n');
+  return `{\n${result}\n}`;
 };
