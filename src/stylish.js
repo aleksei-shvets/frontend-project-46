89dd1711d850794f1c6d2b2e337e09262cc721c6
aleksei-shvetz <al.sv.dev@gmail.com>
2023-08-26 17:19:34 +0300

    Create formater function

---
+++ src/stylish.js
@@ -0,0 +1,18 @@
+export default (tree) => {
+  const iter = (treeArray, level = 1) => {
+    const tab = ' ';
+    const result = treeArray.map((object) => {
+      if (object.type !== 'node') {
+        if (object.type === 'notchanged') {
+          return `${tab.repeat(level)}${object.key}: ${object.value}`;
+        }
+        if (object.type === 'changed') {
+          return `${tab.repeat(level)}- ${object.key}: ${object.value1}\n${tab.repeat(level)}+ ${object.key}: ${object.value2}`;
+        }
+      }
+      return `${tab.repeat(level)}${object.key}: ${iter(object.value, level + 1)}`;
+    }).join('\n');
+    return `{\n${result}\n${tab.repeat(level - 1)}}`;
+  };
+  return iter(tree);
+};
