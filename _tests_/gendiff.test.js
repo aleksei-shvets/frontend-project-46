54b5343d666f95f6b7b83aeabc47b25b5eef014b
aleksei-shvetz <al.sv.dev@gmail.com>
2023-09-04 15:15:27 +0300

    Update test for new functionality

--- _tests_/gendiff.test.js
+++ _tests_/gendiff.test.js
@@ -2,7 +2,8 @@
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import { readFileSync } from 'node:fs';
-import filesCompare from '../src/filesCompare.js';
+import genDiffTree from '../src/genDiffTree.js';
+import formater from '../src/stylish.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,11 +14,13 @@ const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf8');
 test('gendiff output JSON', () => {
   const filePath1 = getFixturePath('file1.json');
   const filePath2 = getFixturePath('file2.json');
-  expect(filesCompare(filePath1, filePath2)).toEqual(readFile('correctOut.txt'));
+  const tree = genDiffTree(filePath1, filePath2);
+  expect(formater(tree)).toEqual(readFile('correctOut.txt'));
 });
 
-test('gendiff output JSON', () => {
-  const filePath1 = getFixturePath('file1.yml');
+test('gendiff output yaml/yml', () => {
+  const filePath1 = getFixturePath('file1.yaml');
   const filePath2 = getFixturePath('file2.yaml');
-  expect(filesCompare(filePath1, filePath2)).toEqual(readFile('correctOut.txt'));
+  const tree = genDiffTree(filePath1, filePath2);
+  expect(formater(tree)).toEqual(readFile('correctOut.txt'));
 });
