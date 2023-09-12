1ddf26359583d8fa175aeb5e3c81574e7e77df23
aleksei-shvetz <al.sv.dev@gmail.com>
2023-09-12 21:34:34 +0300

    Rename gendiff.test.js to stylish.test.js

--- _tests_/stylish.test.js
+++ _tests_/stylish.test.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-underscore-dangle */
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
-import { readFileSync } from 'node:fs';
+import { readFileSync } from 'fs';
 import genDiffTree from '../src/genDiffTree.js';
-import formater from '../src/stylish.js';
+import formater from '../src/formaters/stylish.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,16 +11,16 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf8');
 
-test('gendiff output JSON', () => {
+test('stylish output JSON', () => {
   const filePath1 = getFixturePath('file1.json');
   const filePath2 = getFixturePath('file2.json');
   const tree = genDiffTree(filePath1, filePath2);
-  expect(formater(tree)).toEqual(readFile('correctOut.txt'));
+  expect(formater(tree)).toEqual(readFile('correctStylish.txt'));
 });
 
-test('gendiff output yaml/yml', () => {
+test('stylish output yaml/yml', () => {
   const filePath1 = getFixturePath('file1.yaml');
   const filePath2 = getFixturePath('file2.yaml');
   const tree = genDiffTree(filePath1, filePath2);
-  expect(formater(tree)).toEqual(readFile('correctOut.txt'));
+  expect(formater(tree)).toEqual(readFile('correctStylish.txt'));
 });
