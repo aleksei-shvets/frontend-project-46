53ae25dfd64833b45139433058187ba191a34861
aleksei-shvetz <al.sv.dev@gmail.com>
2023-09-13 18:54:48 +0300

    Fixed all bugs in plain.js

--- src/formaters/plain.js
+++ src/formaters/plain.js
@@ -8,24 +8,25 @@ const valueStringify = (item) => {
   return item;
 };
 
-const getFullKey = (path, key) => (path ? `${path}.${key}` : key);
+const getFullPath = (path, key) => (path ? `${path}.${key}` : key);
 
 export default (tree) => {
-  const iter = (treeArray, preName = '') => {
+  const iter = (treeArray, currentPath = '') => {
     const result = Array.isArray(treeArray) ? treeArray.map((object) => {
       if (object.type !== 'node') {
         if (object.type === 'changed') {
-          return `Property '${getFullKey(preName, object.key)}' was updated. From ${valueStringify(object.value1)} to ${valueStringify(object.value2)}`;
+          return `Property '${getFullPath(currentPath, object.key)}' was updated. From ${valueStringify(object.value1)} to ${valueStringify(object.value2)}`;
         }
         if (object.type === 'added') {
-          return `Property '${getFullKey(preName, object.key)}' was added with value: ${valueStringify(object.value)}`;
+          return `Property '${getFullPath(currentPath, object.key)}' was added with value: ${valueStringify(object.value)}`;
         }
         if (object.type === 'deleted') {
-          return `Property '${getFullKey(preName, object.key)}' was removed`;
+          return `Property '${getFullPath(currentPath, object.key)}' was removed`;
         }
       }
-      return iter(object.value, object.key);
-    }).join('\n') : undefined;
+      const currentLevelPath = getFullPath(currentPath, object.key);
+      return iter(object.value, currentLevelPath);
+    }).filter(Boolean).join('\n') : undefined;
     return result;
   };
   return iter(tree);
