47292cf837a32576738645611531e57c9dce8b01
aleksei-shvetz <al.sv.dev@gmail.com>
2023-09-14 18:37:50 +0300

    Refactoring makeDiffTree

--- src/makeDiffTree.js
+++ src/makeDiffTree.js
@@ -17,24 +17,16 @@ export default (file1, file2) => {
     const tree = _.sortBy(keys, (key) => key)
       .map((key) => {
         if (_.isPlainObject(content1[key]) && _.isPlainObject(content2[key])) {
-          return {
-            type: 'node', key: `${key}`, value: compareFiles(content1[key], content2[key]),
-          };
+          return { type: 'node', key: `${key}`, value: compareFiles(content1[key], content2[key]) };
         }
         if (_.has(content1, key) && !_.has(content2, key)) {
-          return {
-            type: 'deleted', key: `${key}`, value: content1[key],
-          };
+          return { type: 'deleted', key: `${key}`, value: content1[key] };
         }
         if (!_.has(content1, key) && _.has(content2, key)) {
-          return {
-            type: 'added', key: `${key}`, value: content2[key],
-          };
+          return { type: 'added', key: `${key}`, value: content2[key] };
         }
         if (content1[key] === content2[key]) {
-          return {
-            type: 'notchanged', key: `${key}`, value: content1[key],
-          };
+          return { type: 'notchanged', key: `${key}`, value: content1[key] };
         }
         return {
           type: 'changed', key: `${key}`, value1: content1[key], value2: content2[key],
