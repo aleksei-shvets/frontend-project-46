29607d57bde372aaf0be3c69fe28a38430b472a2
aleksei-shvetz <al.sv.dev@gmail.com>
2023-09-14 10:00:12 +0300

    Fix tests

--- _tests_/index.test.js
+++ _tests_/index.test.js
@@ -10,45 +10,30 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf8');
+const filePath1 = getFixturePath('file1.json');
+const filePath2 = getFixturePath('file2.json');
+const filePath3 = getFixturePath('file1.yaml');
+const filePath4 = getFixturePath('file2.yaml');
+const treeFromJson = makeDiffTree(filePath1, filePath2);
+const treeFromYaml = makeDiffTree(filePath3, filePath4);
 
-test('stylish format output for .json', () => {
-  const filePath1 = getFixturePath('file1.json');
-  const filePath2 = getFixturePath('file2.json');
-  const tree = makeDiffTree(filePath1, filePath2);
-  expect(genDiff(tree, 'stylish')).toEqual(readFile('correctStylish.txt'));
+describe('stylish format', () => {
+  test('stylish format output', () => {
+    expect(genDiff(treeFromJson, 'stylish')).toEqual(readFile('correctStylish.txt'));
+    expect(genDiff(treeFromYaml, 'stylish')).toEqual(readFile('correctStylish.txt'));
+  });
 });
 
-test('stylish format output for .yaml/.yml', () => {
-  const filePath1 = getFixturePath('file1.yaml');
-  const filePath2 = getFixturePath('file2.yaml');
-  const tree = makeDiffTree(filePath1, filePath2);
-  expect(genDiff(tree, 'stylish')).toEqual(readFile('correctStylish.txt'));
+describe('plain format', () => {
+  test('plain format output', () => {
+    expect(genDiff(treeFromJson, 'plain')).toEqual(readFile('correctPlain.txt'));
+    expect(genDiff(treeFromYaml, 'plain')).toEqual(readFile('correctPlain.txt'));
+  });
 });
 
-test('plain format output for .json', () => {
-  const filePath1 = getFixturePath('file1.json');
-  const filePath2 = getFixturePath('file2.json');
-  const tree = makeDiffTree(filePath1, filePath2);
-  expect(genDiff(tree, 'plain')).toEqual(readFile('correctPlain.txt'));
-});
-
-test('plain format output for .yaml/.yml', () => {
-  const filePath1 = getFixturePath('file1.yaml');
-  const filePath2 = getFixturePath('file2.yaml');
-  const tree = makeDiffTree(filePath1, filePath2);
-  expect(genDiff(tree, 'plain')).toEqual(readFile('correctPlain.txt'));
-});
-
-test('JSON format output for .json', () => {
-  const filePath1 = getFixturePath('file1.json');
-  const filePath2 = getFixturePath('file2.json');
-  const tree = makeDiffTree(filePath1, filePath2);
-  expect(genDiff(tree, 'json')).toEqual(readFile('correctJsonFormat.txt'));
-});
-
-test('JSON format output for .yaml/.yml', () => {
-  const filePath1 = getFixturePath('file1.yaml');
-  const filePath2 = getFixturePath('file2.yaml');
-  const tree = makeDiffTree(filePath1, filePath2);
-  expect(genDiff(tree, 'json')).toEqual(readFile('correctJsonFormat.txt'));
+describe('JSON format', () => {
+  test('JSON format output', () => {
+    expect(genDiff(treeFromJson, 'json')).toEqual(readFile('correctJsonFormat.txt'));
+    expect(genDiff(treeFromYaml, 'json')).toEqual(readFile('correctJsonFormat.txt'));
+  });
 });
