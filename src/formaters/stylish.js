6fc960daa96666ad1e7fb66f7e5114b1fbb8f1a7
aleksei-shvetz <al.sv.dev@gmail.com>
2023-09-14 18:02:00 +0300

    Refactoring stylish

--- src/formaters/stylish.js
+++ src/formaters/stylish.js
@@ -22,24 +22,27 @@ const stringify = (object, level = 0) => {
 
 export default (tree) => {
   const iter = (treeArray, level = 1, spaceCount = 4) => {
+    const standardIndent = genIndent(spaceCount * level);
+    const offsetIndent = genIndent(spaceCount * level - 2);
+    const closingIndent = genIndent(spaceCount * (level - 1));
     const result = treeArray.map((object) => {
       if (object.type !== 'node') {
         switch (object.type) {
           case 'notchanged':
-            return `${genIndent(spaceCount * level)}${object.key}: ${stringify(object.value, level)}`;
+            return `${standardIndent}${object.key}: ${stringify(object.value, level)}`;
           case 'changed':
-            return `${genIndent(spaceCount * level - 2)}- ${object.key}: ${stringify(object.value1, level)}\n${genIndent(spaceCount * level - 2)}+ ${object.key}: ${stringify(object.value2)}`;
+            return `${offsetIndent}- ${object.key}: ${stringify(object.value1, level)}\n${offsetIndent}+ ${object.key}: ${stringify(object.value2)}`;
           case 'added':
-            return `${genIndent(spaceCount * level - 2)}+ ${object.key}: ${stringify(object.value, level)}`;
+            return `${offsetIndent}+ ${object.key}: ${stringify(object.value, level)}`;
           case 'deleted':
-            return `${genIndent(spaceCount * level - 2)}- ${object.key}: ${stringify(object.value, level)}`;
+            return `${offsetIndent}- ${object.key}: ${stringify(object.value, level)}`;
           default:
             throw new Error(`Unknown node in tree ${tree}`);
         }
       }
-      return `${genIndent(spaceCount * level)}${object.key}: ${iter(object.value, level + 1)}`;
+      return `${standardIndent}${object.key}: ${iter(object.value, level + 1)}`;
     }).join('\n');
-    return `{\n${result}\n${genIndent(spaceCount * (level - 1))}}`;
+    return `{\n${result}\n${closingIndent}}`;
   };
   return iter(tree);
 };
