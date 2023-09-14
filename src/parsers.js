294e0bafed211a7516757e8fe96319d3e1febd0e
aleksei-shvetz <al.sv.dev@gmail.com>
2023-09-14 15:13:45 +0300

    Refactoring parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -2,13 +2,11 @@
 import yaml from 'js-yaml';
 
 export default (file, extention) => {
-  let fileContent;
   if (extention === '.yml' || extention === '.yaml') {
-    fileContent = yaml.load(file);
-  } else if (extention === '.json') {
-    fileContent = JSON.parse(file);
-  } else {
-    throw new Error(`${extention} - extention not supported`);
+    return yaml.load(file);
   }
-  return fileContent;
+  if (extention === '.json') {
+    return JSON.parse(file);
+  }
+  throw new Error(`${extention} - extention not supported`);
 };
