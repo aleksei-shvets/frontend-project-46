c37994837dc1664c14822f80a611380765609496
aleksei-shvetz <al.sv.dev@gmail.com>
2023-09-15 10:23:17 +0300

    Create src/index.js with main function
    to use package as library

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import { readFileSync } from 'fs';
+import { cwd } from 'process';
+import path from 'path';
+import makeDiffTree from './makeDiffTree.js';
+import fileParse from './parsers.js';
+import formatter from './formatters/index.js';
+
+const readFile = (file) => readFileSync(path.resolve(cwd(), file), 'utf-8');
+
+export default (file1, file2, format = 'stylish') => {
+  const extension1 = path.extname(file1);
+  const extension2 = path.extname(file2);
+  const fileContent1 = fileParse(readFile(file1), extension1);
+  const fileContent2 = fileParse(readFile(file2), extension2);
+  const tree = makeDiffTree(fileContent1, fileContent2);
+  return formatter(tree, format);
+};
