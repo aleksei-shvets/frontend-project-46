ed10db5da614e0c0303bbff9238d296d56299be3
aleksei-shvetz <al.sv.dev@gmail.com>
2023-09-15 12:36:56 +0300

    Mini refactoring src/index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import formatter from './formatters/index.js';
 
 const readFile = (file) => readFileSync(path.resolve(cwd(), file), 'utf-8');
 
-const genDiff = (file1, file2, format = 'stylish') => {
+export default (file1, file2, format = 'stylish') => {
   const extension1 = path.extname(file1);
   const extension2 = path.extname(file2);
   const fileContent1 = fileParse(readFile(file1), extension1);
@@ -15,4 +15,3 @@ const genDiff = (file1, file2, format = 'stylish') => {
   const tree = makeDiffTree(fileContent1, fileContent2);
   return formatter(tree, format);
 };
-export default genDiff;
