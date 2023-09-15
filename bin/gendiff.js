621de3e2ab4621ef3d862e7e306ed82fb58801dd
aleksei-shvetz <al.sv.dev@gmail.com>
2023-09-15 09:09:28 +0300

    Some changes
    move index.js to formaters
    renamed formaters to formatters
    fix imports

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import genDiff from '../src/index.js';
+import genDiff from '../src/formatters/index.js';
 import makeDiffTree from '../src/makeDiffTree.js';
 
 const program = new Command();
