1e1654403fccfdf7595cc42196869ff2e280b593
aleksei-shvetz <al.sv.dev@gmail.com>
2023-09-18 22:24:44 +0300

    Rename function to makeDiffTree from comareFiles

--- src/makeDiffTree.js
+++ src/makeDiffTree.js
@@ -1,11 +1,11 @@
 import _ from 'lodash';
 
-const compareFiles = (content1, content2) => {
+const makeDiffTree = (content1, content2) => {
   const keys = Object.keys({ ...content1, ...content2 });
   const tree = _.sortBy(keys, (key) => key)
     .map((key) => {
       if (_.isPlainObject(content1[key]) && _.isPlainObject(content2[key])) {
-        return { type: 'node', key: `${key}`, value: compareFiles(content1[key], content2[key]) };
+        return { type: 'node', key: `${key}`, value: makeDiffTree(content1[key], content2[key]) };
       }
       if (_.has(content1, key) && !_.has(content2, key)) {
         return { type: 'deleted', key: `${key}`, value: content1[key] };
@@ -23,4 +23,4 @@ const compareFiles = (content1, content2) => {
   return tree;
 };
 
-export default compareFiles;
+export default makeDiffTree;
