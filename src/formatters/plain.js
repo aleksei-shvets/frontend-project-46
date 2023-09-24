41fcc0580a755aa0a6e00fe58714cb22c324302f
aleksei-shvetz <al.sv.dev@gmail.com>
2023-09-24 14:39:05 +0300

    Refactoring plain (add regex)

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,6 +1,7 @@
 const valueStringify = (item) => {
   if (item instanceof Object) return '[complex value]';
   if (typeof item === 'string') return `'${item}'`;
+  if (item === null) return item;
   return item;
 };
 
@@ -11,20 +12,20 @@ export default (tree) => {
     const result = treeArray.map((object) => {
       switch (object.type) {
         case 'changed':
-          return `Property '${getFullPath(currentPath, object.key)}' was updated. From ${valueStringify(object.value1)} to ${valueStringify(object.value2)}`;
+          return `Property '${getFullPath(currentPath, object.key)}' was updated. From ${valueStringify(object.value1)} to ${valueStringify(object.value2)}\n`;
         case 'added':
-          return `Property '${getFullPath(currentPath, object.key)}' was added with value: ${valueStringify(object.value)}`;
+          return `Property '${getFullPath(currentPath, object.key)}' was added with value: ${valueStringify(object.value)}\n`;
         case 'deleted':
-          return `Property '${getFullPath(currentPath, object.key)}' was removed`;
+          return `Property '${getFullPath(currentPath, object.key)}' was removed\n`;
         case 'nested':
-          return iter(object.children, getFullPath(currentPath, object.key));
+          return `${iter(object.children, getFullPath(currentPath, object.key))}\n`;
         case 'notchanged':
           return null;
         default:
           throw new Error(`Unknown node in tree ${tree}`);
       }
-    }).join('\n');
-    return result;
+    }).join('');
+    return result.replace(/\n(?=\s*)$/gm, '');
   };
   return iter(tree);
 };
