4840c9f182a97dbab7b06351d8fac9323ff8d81b
aleksei-shvetz <al.sv.dev@gmail.com>
2023-09-25 11:40:49 +0300

    Remove helpoption in gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,7 +9,6 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
-  .helpOption('-h, --help', 'output usage information')
   .option('-f, --format <type>', 'output format', 'stylish')
   .action((filePath1, filePath2) => {
     console.log(genDiff(filePath1, filePath2, program.opts().format));
