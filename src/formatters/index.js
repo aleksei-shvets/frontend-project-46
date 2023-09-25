b6b3b8d4c7183d3c2d9266588fcf1e0e61ab75c9
aleksei-shvetz <al.sv.dev@gmail.com>
2023-09-25 21:43:59 +0300

    Refactoring src/formatters/index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,14 +2,14 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 
 export default (tree, formatName) => {
-  switch (formatName) {
-    case 'stylish':
-      return stylish(tree);
-    case 'plain':
-      return plain(tree);
-    case 'json':
-      return JSON.stringify(tree);
-    default:
-      throw new Error('Unknown format');
+  const formaters = {
+    stylish: () => stylish(tree),
+    plain: () => plain(tree),
+    json: () => JSON.stringify(tree),
+  };
+
+  if (formaters[formatName]) {
+    return formaters[formatName]();
   }
+  throw new Error(`${formatName} - Unsupported format`);
 };
